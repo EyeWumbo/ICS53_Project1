22b61de8e4e8141bd73b6398605f2786ba2f2fb0
truongmm <truongmm@uci.edu>
2014-05-05 19:18:01 -0700

    fixed errors with directory method

--- ICS51_TestApp_VS/ICS51_TestApp_VS/FileSystem.cpp
+++ ICS51_TestApp_VS/ICS51_TestApp_VS/FileSystem.cpp
@@ -10,29 +10,29 @@ FileSystem::FileSystem(IOSystem* iosystem){
 	this->iosystem = iosystem;
 
 	for(int i = 0; i < 14; i ++){
-		directory[i] = nullptr;
+		dir[i] = nullptr;
 	}
 	for(int i = 0; i < 3; i ++){
 		openFileTable[i] = OFT();
 	}
 }
 
-int FileSystem::open(std::string symbolicName){
+/* int FileSystem::open(std::string symbolicName){
 	for (int i = 0; i < 14; i++){
 		for (int j = 0; symbolicName[j] != 0; j++){
 			if ()
 		}
 	}
 	return -1;
-}
+} */
 
 void FileSystem::directory(){
 	for (int i=0; i < 14; i++){
-		dirEntry entry = dir[i];
-		if (entry != nullptr){
-			char* fileDesc[64];
-			iosystem.read_block(entry.indexForDesc, fileDesc);
-			std::cout << entry.symbolic_file_name << " " << fileDesc[0] << std::endl;
+		dirEntry* entry = dir[i];
+		if (entry){
+			char* fileDesc = new char[64];
+			iosystem->read_block(entry->indexForDesc, fileDesc);
+			std::cout << entry->symbolic_file_name << " " << fileDesc[0] << std::endl;
 		}
 	}
 }
